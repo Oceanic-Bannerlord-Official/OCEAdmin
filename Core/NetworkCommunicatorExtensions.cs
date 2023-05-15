4f3cfba4f2b19a54a97e87b63643b0421c3339c3
zacharyenriquee <zacharyenriquee@gmail.com>
2023-05-15 17:11:01 +1000

    ADDITION: TeamComposition, GetSpecs command, prints all classes across teams

--- Core/NetworkCommunicatorExtensions.cs
+++ Core/NetworkCommunicatorExtensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
 namespace OCEAdmin
@@ -33,6 +34,26 @@ namespace OCEAdmin
         public static bool IsMuted(this NetworkCommunicator networkPeer)
         {
             return networkPeer.GetPlayerExtensionComponent().IsMuted();
+        }
+
+        public static bool IsCavalry(this NetworkCommunicator networkPeer)
+        {
+            return !networkPeer.ControlledAgent.SpawnEquipment[EquipmentIndex.Horse].IsEmpty;
+        }
+
+        public static bool IsArcher(this NetworkCommunicator networkPeer)
+        {
+            return networkPeer.ControlledAgent.Equipment.HasRangedWeapon() && !networkPeer.IsCavalry();
+        }
+
+        public static bool IsInfantry(this NetworkCommunicator networkPeer)
+        {
+            return !networkPeer.IsArcher() && !networkPeer.IsCavalry();
+        }
+
+        public static void SendChatMessage(this NetworkCommunicator networkPeer, string message)
+        {
+            MPUtil.SendChatMessage(networkPeer, message);
         }
     }
 }
